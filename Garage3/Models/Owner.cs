2ed0a927dd400da09e984ee5dff034037f3944de
David-Lundmark <david.lundmarkbredband.net>
2016-11-23 15:04:43 +0100

    Model

--- Garage3/Models/Owner.cs
+++ Garage3/Models/Owner.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Garage3.Models
 {
     public class Owner
     {
+        [Key]
         public string Owner_ID { get; set; }
         public string Name { get; set; }
         [ForeignKey("Vehicle")]
--- Garage3/Models/Vehicle.cs
+++ Garage3/Models/Vehicle.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Garage3.Models
 {
     public class Vehicle
     {
+        [Key]
         public string Vehicle_ID { get; set; }
         [ForeignKey("Owner")]
         public virtual Owner Owner_ID { get; set; }
         [ForeignKey("VehicleType")]
-        public int Vehicletype { get; set; }
+        public int VehicleType { get; set; }
     }
 }
